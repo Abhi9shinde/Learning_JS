49a718a02620cd57dc22c0116bfe36797d8b92dd
Abhinav <2004abhishinde@gmail.com>
2024-06-15 23:40:42 +0530

    brush_UP

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-const input = document.querySelector('#username');
-const h1 = document.querySelector('h1');
-input.addEventListener('input', function (e) {
-    h1.innerText = `Welcome, ${input.value}`;
-});
+console.log("Hello THERE")
